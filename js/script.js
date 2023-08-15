b3bdcfb068d868d70f4a3bd3f1e7c2feed2bdb77
Lost-in-Noise <nancykikvadze@gmail.com>
2023-08-15 23:39:33 +0400

    for loop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // დავალება 
 // 1.
-for (let i = 5; i <= 100; i++) {
-    console.log(i); 
+// for (let i = 5; i <= 100; i++) {
+//     console.log(i); 
+//   }
+
+//   2.
+let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] > 0 && array1[i] < 10) {
+    console.log(array1[i]); 
   }
-  
+}
