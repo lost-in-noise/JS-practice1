33ff823632918fb4b16634846ff787d9cb8361e3
Lost-in-Noise <nancykikvadze@gmail.com>
2023-08-15 23:43:33 +0400

    loop

--- js/script.js
+++ js/script.js
@@ -5,10 +5,21 @@
 //   }
 
 //   2.
-let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
 
-for (let i = 0; i < array1.length; i++) {
-  if (array1[i] > 0 && array1[i] < 10) {
-    console.log(array1[i]); 
-  }
+// for (let i = 0; i < array1.length; i++) {
+//   if (array1[i] > 0 && array1[i] < 10) {
+//     console.log(array1[i]); 
+//   }
+// }
+
+// 3.
+let array3 = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < array3.length; i++) {
+  sum += array3[i];
 }
+
+console.log(sum);
+
